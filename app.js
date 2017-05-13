b4f45486f262e450ed651f25298f78ca32859945
Rajeev Jayaswal <justjayaswal@gmail.com>
2017-05-13 18:54:02 +0530

    changed port

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ io.on('connection', function(socket){
 
 });
 
-
-http.listen(8080, function(){
-  console.log('listening on:8080');
+var port = process.env.PORT || 8080;
+http.listen(port, function(){
+  console.log('listening on:' + port);
 });
